ac8d1c24b9744cafd81dcc0e48a05f253295b31e
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-11-21 16:11:54 +0530

    helper

---
+++ src/helpers/index.ts
@@ -0,0 +1,10 @@
+import crypto from 'crypto'
+
+const SECRET = 'REST-API'
+
+//helper for authentication 
+
+export const random = () => crypto.randomBytes(128).toString('base64')
+export const authentication = (salt : string , password : string) => {
+    return crypto.createHmac('sha256' , [salt,password].join('/')).update(SECRET).digest('hex');
+}
