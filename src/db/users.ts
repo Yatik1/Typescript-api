170e6e1ce785b2a50828b1502ac011a3408aeb6d
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-11-21 15:32:44 +0530

    database schema

---
+++ src/db/users.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+   username : { type : String , required : true },
+   email : { type:String , required : true } , 
+   authentication : {
+    password : { type : String , required : true , select: false } ,
+    salt : { type : String , required : true , select : false } ,
+    sessionToken : { type : String , required : true, select : false},
+   }
+ })
+
+ export const UserModel = mongoose.model("User" , UserSchema)
+
+
