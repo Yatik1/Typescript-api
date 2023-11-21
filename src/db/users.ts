dcb4ca814d0a7ac474ff81c7ecc0138c73cd1c98
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-11-21 21:55:58 +0530

    ::schema

--- src/db/users.ts
+++ src/db/users.ts
@@ -5,8 +5,8 @@ const UserSchema = new mongoose.Schema({
    email : { type:String , required : true } , 
    authentication : {
     password : { type : String , required : true , select: false } ,
-    salt : { type : String , required : true , select : false } ,
-    sessionToken : { type : String , required : true, select : false},
+    salt : { type : String , select : false } ,
+    sessionToken : { type : String , select : false},
    }
  })
 
@@ -15,12 +15,9 @@ const UserSchema = new mongoose.Schema({
 //actions 
 
 export const getUsers = () => UserModel.find();
-export const getUserByEmail = (email : string) => UserModel.findOne({email});
-export const getUserBySessoinToken = (sessionToken : string) => UserModel.findOne({
-   'authentication.sessionToken' : sessionToken , 
-})
-export const getUserById = (id:string) => UserModel.findById(id)
-
-export const createUser = (values : Record<string,any>) => new UserModel(values).save().then((user) => user.toObject())
-export const deleteUserById = (id:string) => UserModel.findOneAndDelete({_id : id})
-export const updateUserById = (id:string , values : Record<string,any>) => UserModel.findByIdAndUpdate(id,values)
+export const getUserByEmail = (email: string) => UserModel.findOne({ email });
+export const getUserBySessionToken = (sessionToken: string) => UserModel.findOne({ 'authentication.sessionToken': sessionToken });
+export const getUserById = (id: string) => UserModel.findById(id);
+export const createUser = (values: Record<string, any>) => new UserModel(values).save().then((user) => user.toObject());
+export const deleteUserById = (id: string) => UserModel.findOneAndDelete({ _id: id });
+export const updateUserById = (id: string, values: Record<string, any>) => UserModel.findByIdAndUpdate(id, values);
