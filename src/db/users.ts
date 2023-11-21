2759f3f756d2d6894bdcb00f8cbd205551f64e9b
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-11-21 16:11:46 +0530

    ::action

--- src/db/users.ts
+++ src/db/users.ts
@@ -12,4 +12,15 @@ const UserSchema = new mongoose.Schema({
 
  export const UserModel = mongoose.model("User" , UserSchema)
 
+//actions 
 
+export const getUsers = () => UserModel.find();
+export const getUserByEmail = (email : string) => UserModel.findOne({email});
+export const getUserBySessoinToken = (sessionToken : string) => UserModel.findOne({
+   'authentication.sessionToken' : sessionToken , 
+})
+export const getUserById = (id:string) => UserModel.findById(id)
+
+export const createUser = (values : Record<string,any>) => new UserModel(values).save().then((user) => user.toObject())
+export const deleteUserById = (id:string) => UserModel.findOneAndDelete({_id : id})
+export const updateUserById = (id:string , values : Record<string,any>) => UserModel.findByIdAndUpdate(id,values)
