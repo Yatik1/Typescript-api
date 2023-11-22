270038ef699a127068be514447608b497cb7868c
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-11-22 12:28:05 +0530

    ::delete

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -2,6 +2,28 @@ import { getUserBySessionToken } from "../db/users";
 import express from "express"
 import {get,merge} from "lodash"
 
+export const isOwner = async (req:express.Request , res:express.Response , next:express.NextFunction) => {
+    try {
+        
+       const {id} = req.params;
+       const currentUserId = get(req , 'identity._id') as string
+
+       if (!currentUserId) {
+        return res.sendStatus(403)
+       }
+
+       if (currentUserId.toString() !== id) {
+         return res.sendStatus(403) 
+       }
+
+       next()
+
+    } catch (error) {
+        console.log(error)
+        return res.sendStatus(400) 
+    }
+}
+
 export const isAuthenticated = async (req:express.Request , res:express.Response , next: express.NextFunction) => {
     try {
         
