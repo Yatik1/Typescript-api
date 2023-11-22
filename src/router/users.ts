4a37eb411a84ea6c2387ab1ba1e6a9ddfdfa4fff
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-11-22 12:47:41 +0530

    routes

--- src/router/users.ts
+++ src/router/users.ts
@@ -1,8 +1,9 @@
 import express from "express"
-import { deleteUser, getAllUsers } from "../controllers/users"
+import { deleteUser, getAllUsers, updateUser } from "../controllers/users"
 import { isAuthenticated, isOwner } from "../middlewares"
 
 export default (router : express.Router) => {
     router.get('/users' , isAuthenticated ,getAllUsers)
     router.delete('/users/:id' ,isAuthenticated , isOwner , deleteUser)
+    router.patch("/users/:id" , isAuthenticated ,isOwner , updateUser)
 }
