225136dd1ce1936c90cf0d3772103ab0603c4538
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-11-22 11:44:03 +0530

    ::middleware

---
+++ src/middlewares/index.ts
@@ -0,0 +1,27 @@
+import { getUserBySessionToken } from "../db/users";
+import express from "express"
+import {get,merge} from "lodash"
+
+export const isAuthenticated = async (req:express.Request , res:express.Response , next: express.NextFunction) => {
+    try {
+        
+      const sessionToken = req.cookies['YATIK_AUTH'];
+
+      if(!sessionToken) {
+        return res.sendStatus(403)
+      }
+
+      const existingUser = await getUserBySessionToken(sessionToken)
+
+      if(!existingUser) {
+        return res.sendStatus(403)
+      }
+
+      merge(req , {identity : existingUser})
+      return next();
+
+    } catch (error) {
+        console.log(error)
+        return res.sendStatus(400) 
+    }
+}
