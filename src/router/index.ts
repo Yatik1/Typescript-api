5718d948c1c996ec3c9ae1ccf6ee26eae7792a2d
Yatik Srivastava <124453837+Yatik1@users.noreply.github.com>
2024-01-15 22:49:37 +0530

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,11 +2,10 @@ import express from "express";
 import authentication from "./authentication";
 import users from "./users";
 
-
 const router = express.Router()
 
 export default (): express.Router => {
-
+    
     authentication(router);
     users(router)
     
